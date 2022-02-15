bea6deb823579daa2d1cd7d13ba6e9734275d761
Mr'Atsadawut <ng.atsadawut@gmail.com>
2022-02-15 18:20:50 +0700

    Exercise5_2

---
+++ Exercise5_2_ATSADAWUT_N.py
@@ -0,0 +1,10 @@
+print("Calculate Velocity from formula V=S/T")
+speed = int(input("input speed : "))
+time = int(input("input time : "))
+print("===========================")
+print("speed : ", speed)
+print("time : ", time)
+print("velocity : ", speed/time)
+print("===========================")peed :  300
+time :  4
+velocity :  75.0
