48ee56f085c85650a4272c289ad0085d8ec461b9
Mr'Atsadawut <ng.atsadawut@gmail.com>
2022-02-15 14:49:51 +0700

    commit exercise4

---
+++ Exercise4_ATSADAWUT_N.py
@@ -0,0 +1,12 @@
+
+
+scoreEnglish = 80.5
+scoreBusiness = 60.5
+scoreComputer = 73
+scoreProgramming = 84.2
+
+print("--- Your Score ---")
+print("Foundation English :", scoreEnglish)
+print("General Business :", scoreBusiness)
+print("Introduction to Computer Systems :", scoreComputer)
+print("Computer Programming :", scoreProgramming)
